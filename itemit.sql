2a2cd5ae8a73c04e246c3d7b0eee2cd69f290d25
KarpoVelho <40266114+KarpoVelho@users.noreply.github.com>
2021-09-02 16:54:48 +0300

    Create itemit.sql

---
+++ itemit.sql
@@ -0,0 +1,6 @@
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
+	('lapio', 'Lapio', -1, 0, 1),
+    ('rolex', 'Rolex', -1, 0, 1),
+    ('donitsi', 'Donitsi', -1, 0, 1),
+    ('koru', 'Timantti-koru', -1, 0, 1),
+    ('pirtelo', 'Pirtelö', -1, 0, 1);
